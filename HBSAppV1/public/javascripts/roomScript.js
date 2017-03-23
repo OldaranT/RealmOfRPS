d795631e95193cb3f7613a788c430456c0458c22
Nick Kronenberg <nikro412956@gmail.com>
2017-03-23 12:31:07 +0100

    username word nu gelezen uit de url en de server geeft de usernames van iedereen in de room;

--- HBSAppV1/public/javascripts/roomScript.js
+++ HBSAppV1/public/javascripts/roomScript.js
@@ -35,7 +35,10 @@ $( document ).ready(function() {
         })
     );
     socket.on('connect', function(){
-        socket.emit('adduser', prompt("What's your name: "));
+        var url = window.location.href;
+        var params = parseURLParams(url);
+
+        socket.emit('adduser', params.name);
     });
 
     socket.on('updatechat', function (username, data) {
@@ -119,6 +122,26 @@ $( document ).ready(function() {
         for (var d in data)
             ret.push(encodeURIComponent(d) + '=' + encodeURIComponent(data[d]));
         return ret.join('&');
+    }
+
+    function parseURLParams(url) {
+        var queryStart = url.indexOf("?") + 1,
+            queryEnd   = url.indexOf("#") + 1 || url.length + 1,
+            query = url.slice(queryStart, queryEnd - 1),
+            pairs = query.replace(/\+/g, " ").split("&"),
+            parms = {}, i, n, v, nv;
+
+        if (query === url || query === "") return;
+
+        for (i = 0; i < pairs.length; i++) {
+            nv = pairs[i].split("=", 2);
+            n = decodeURIComponent(nv[0]);
+            v = decodeURIComponent(nv[1]);
+
+            if (!parms.hasOwnProperty(n)) parms[n] = [];
+            parms[n].push(nv.length === 2 ? v : null);
+        }
+        return parms;
     }
 
 });
